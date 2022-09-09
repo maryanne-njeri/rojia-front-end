cbd5c2229bae10b2415be648170ed116492befcb
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-09-09 16:31:53 +0300

    adds project managers component

---
+++ src/components/ProjectManagers.js
@@ -0,0 +1,12 @@
+import PostForm from "./PostForm";
+
+function ProjectManagers() {
+    return(
+        <div>
+            <PostForm/>
+        </div>
+    )
+}
+
+
+export default ProjectManagers;
